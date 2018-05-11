dc9ab498eeddffa144cf2bd9f0d4df827c1129aa
ledungsbacken <ledungsbacken@gmail.com>
2018-05-12 00:33:56 +0200

    Fixing fontend

--- resources/views/layouts/admin_nav.blade.php
+++ resources/views/layouts/admin_nav.blade.php
@@ -3,7 +3,7 @@
         <div class="navbar-header">
 
             <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-admin-navbar-collapse" aria-expanded="false">
                 <span class="sr-only">Toggle Navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -14,7 +14,7 @@
             <a class="navbar-brand" href="/">Admin</a>
         </div>
 
-        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+        <div class="collapse navbar-collapse" id="app-admin-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
                 &nbsp;
