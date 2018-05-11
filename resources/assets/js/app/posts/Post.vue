63cc1e01776e4d63212a60296161caef50264b5f
Daniel93 <ljungqvist1993@gmail.com>
2018-05-12 01:21:18 +0200

    Latest post styled

--- resources/assets/js/app/posts/Post.vue
+++ resources/assets/js/app/posts/Post.vue
@@ -1,21 +1,43 @@
 <template>
     <div>
-        <div>Title: {{ post.data.title }}</div>
-        <div>Author: {{ post.user.data.name }}</div>
-        <div>Views: {{ post.data.views }}</div>
-        <div>Posted: {{ post.dateToString(post.data.published) }}</div>
-        <div><a :href="'/post/'+post.data.slug">Link</a></div>
-        <div v-for="subcategory in post.subcategories">
-            Category:
-            <a :href="'/category/' + subcategory.category.data.slug">
-                {{subcategory.category.data.name}}
-            </a>
-            /
-            <a :href="'/category/' + subcategory.category.data.slug + '/' + subcategory.data.slug">
-                {{ subcategory.data.name }}
-            </a>
-        </div>
-        <br />
+        <article class="col">
+            <div class="hoverColors">
+                <a :href="'/post/'+post.data.slug" class="posts">
+                    <img :src="post.headImage.data.thumbnail" alt="">
+                    <header>
+                        <h2>{{ post.data.title }}</h2>
+                        <p>{{ post.data.subtitle }}</p>
+                    </header>
+                </a>
+                <ul class="inline" id="laptop">
+                    <li>
+                        by <strong>
+                                <a :href="'/user/' + post.user.data.id">
+                                    {{ post.user.data.name }}
+                                </a>
+                            </strong> in 
+                            <strong>
+                                <div v-for="subcategory in post.subcategories">
+                                    <a :href="'/category/' + subcategory.category.data.slug">
+                                        {{ subcategory.category.data.name }}
+                                    </a>
+                                </div>
+                            </strong>
+                        </li>
+                    <li>{{ post.dateToString(post.data.published) }}</li>
+                </ul>
+                <ul class="inline" id="mobile">
+                    <li>
+                        <strong>
+                            <a :href="'/user/' + post.user.data.id">
+                                {{ post.user.data.name }}
+                            </a>
+                        </strong>
+                    </li>
+                    <li>{{ post.dateToString(post.data.published) }}</li>
+                </ul>
+            </div>
+        </article>
     </div>
 </template>
 
