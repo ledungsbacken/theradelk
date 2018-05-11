37bdc73bd4f9fda98fef49d262032d7a5739fd7e
Daniel93 <ljungqvist1993@gmail.com>
2018-05-12 01:30:04 +0200

    Yes!

--- resources/views/post/latest.blade.php
+++ resources/views/post/latest.blade.php
@@ -1,15 +1,16 @@
 @extends('master')
 @section('content')
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Latest</div>
-
-                <div class="panel-body">
-                    @each('includes.post', $posts, 'post')
-                    <postload></postload>
-                </div>
-            </div>
-        </div>
-    </div>
+<div id="startPage">
+    <section id="latest">
+        <header>
+            <h1 id="subtitle">
+                Recent
+            </h1>
+        </header>
+        <main>
+            @each('includes.post', $posts, 'post')
+            <postload></postload>
+        </main>
+    </section>
+</div>
 @endsection
