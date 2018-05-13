3dfd16928f6127307affc5cc27ca9354125a0b76
ledungsbacken <ledungsbacken@gmail.com>
2018-05-13 18:14:31 +0200

    Updating the update function to have the same logic as create

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -190,25 +190,46 @@ class PostController extends Controller
      * @return Post
      */
     public function update(Request $request, $id) {
+        $validatedData = $request->validate([
+            'title' => 'max:255',
+            'subtitle' => 'max:255',
+        ]);
+
         $post = Post::find((int)$id);
         // Return 403 if not enough permissions
         if(Auth::user()->cant('update', $post)) { return response()->json('Forbidden', 403); }
 
-        $slug = str_replace([' ', 'å', 'ä', 'ö', 'Å', 'Ä', 'Ö'], ['-', 'a', 'a', 'o', 'A', 'A', 'O'], strtolower($request->title));
-        $post->update([
-            'title' => $request->title,
-            'subtitle' => $request->subtitle,
-            'content' => $request->content,
-            'slug' => $slug,
-            'opacity' => $request->opacity,
-            'is_fullscreen' => $request->is_fullscreen,
-        ]);
-        $post->headImage()->associate((int)$request->head_image_id)->save();
-        $subcategories = [];
-        foreach($request->get('subcategories') as $subcategory) {
-            $subcategories[] = $subcategory['data']['id'];
+        DB::beginTransaction();
+        try {
+            $title = $request->title ?: '';
+            $subtitle = $request->subtitle ?: '';
+            $content = $request->content ?: '';
+            $slug = $request->title ? getSlug($request->title) : getSlug(uniqid(rand(1, 100)));
+
+            $post->update([
+                'title' => $title,
+                'subtitle' => $subtitle,
+                'content' => $content,
+                'slug' => $slug,
+                'opacity' => $request->opacity,
+                'is_fullscreen' => (Bool)$request->is_fullscreen ?: false,
+            ]);
+            if($request->head_image_id) {
+                $post->headImage()->associate((int)$request->head_image_id)->save();
+            }
+            $subcategories = [];
+            foreach($request->get('subcategories') as $subcategory) {
+                $subcategories[] = $subcategory['data']['id'];
+            }
+            $post->subcategories()->sync($subcategories);
+            DB::commit();
+        } catch(\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        } catch(\Throwable $e) {
+            DB::rollBack();
+            throw $e;
         }
-        $post->subcategories()->sync($subcategories);
 
         return $post->load('subcategories.category', 'user', 'headImage');
     }
